7b3ed8e8fdad0c46ba89f7bcfc1b1202c3ec6098
HieuBui <hieuitdevs@gmail.com>
2023-12-06 22:42:33 +0700

    try postgres

--- database.py
+++ database.py
@@ -6,10 +6,9 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from config import setting
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}"
-print(setting)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"postgres://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}"
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SeesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
